9750769d1ddee409231be09198f9f8d4ea5e64e3
佐藤彩美 <a-satou@cmind-co.jp>
2020-08-22 22:47:23 +0900

    test

---
+++ app/Http/Controllers/Admin/ProfilleController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfilleController extends Controller
+{
+    //記入
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create()
+    {
+        return rediect('admin/profile/create');
+    }
+
+    public function edit()
+    {
+        return view('admin/profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+}
